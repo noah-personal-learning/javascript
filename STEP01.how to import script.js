6be810056c4f6342e89b9e7ac4474677928aae45
박은우 <lostist@MacBook-Pro.local>
2021-04-19 20:17:35 +0900

    STEP02

---
+++ STEP01.how to import script.js
@@ -0,0 +1,4 @@
+// 1. Use strict
+// added ECMAScript 5
+// 정의 되지 않은 변수를 처리 하지 않는다는 의미.
+'use strict';
---
+++ STEP02.how to use variable.js
@@ -0,0 +1,15 @@
+// 2. Variable
+// let (added in ES6)
+let globalName = 'Global Name';
+{
+    let name = 'noah';
+    console.log(' block in name 1 : ' + name);
+
+    name = 'haon';
+    console.log(' block in name 2 : ' + name);
+
+    // 전역변수
+    console.log(' block in global name : ' + globalName);
+}
+console.log(' block out name : ' + name);
+console.log(' block out global name : ' + globalName);
