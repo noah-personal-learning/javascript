ba05be7f8a8078026c050b15d26bc428364b4779
ieunune <50124623+ieunune@users.noreply.github.com>
2021-05-16 21:50:18 +0900

    자료정리

--- STEP05.what is fuction.js
+++ STEP05.what is fuction.js
@@ -9,3 +9,39 @@
 function printHello() {
   console.log(`Hello`);
 }
+
+printHello();
+
+function log(msg) {
+  console.log(msg);
+}
+
+log("파라미터 로그");
+
+/* 타입 스크립트
+// 함수명(파라미터:데이터타입):리턴타입
+function log(msg:string):number{
+    console.log(msg);
+    return 0;`
+}
+*/
+
+// 2. Parameters
+// premitive parameters: passed by value
+// object parameters: passed by reference
+function changeName(obj) {
+  obj.name = 'haon';
+}
+
+// name속성에 noah라는 value값을 가진 noah 오브젝트가 메모리에 적재
+const noah = { name : 'noah'};
+// changeName 함수에 noah Object 전달
+changeName(noah);
+// 참조하고 있는 메모리 수정되어 haon 출력.
+console.log(noah);
+
+// 3. Default parameters (added in ES6)
+function showMessage(message, from) {
+  console.log(`${message} by ${from}`);
+}
+showMessage('Hi!');
